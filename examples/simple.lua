768ae66969971792b532ebfacb8c0f9e58bbd572
kraftwerk28 <kefirchik3@gmail.com>
2023-01-21 02:50:07 +0200

    Send payload instead of message in :send await

--- examples/simple.lua
+++ examples/simple.lua
@@ -3,11 +3,9 @@
 local i3 = require("i3ipc")
 local ipc = i3.Connection:new()
 ipc:main(function()
-  local ret = ipc:command([[exec notify-send "Hello, from i3ipc"]])
+  local ret = ipc:get_inputs()
   print(require("inspect")(ret))
-  local count = 0
   ipc:on("window", function(_, event)
-    count = count + 1
     print(require("inspect")(event))
   end)
 end)
